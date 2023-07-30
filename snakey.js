631cc86293e9c88f9e2c5e8b35fe8a1acde9d48c
andys <andysmith836@gmail.com>
2023-07-30 21:46:48 +0100

    Bit of code to store the positions of all elements of the snake

---
+++ snakey.js
@@ -0,0 +1,19 @@
+function Snake(size) {
+
+    let elements = [];
+
+    function updatePosition(x, y) {
+        elements.push({x: x, y: y});
+        if (elements.length > size) {
+            elements = elements.slice(1);
+        }
+    }
+
+    function getElements() {
+        return elements;
+    }
+
+    return {addElement: updatePosition, getElements};
+}
+
+module.exports = { Snake };
---
+++ snakey.spec.js
@@ -0,0 +1,91 @@
+const Snake = require("./snakey").Snake;
+
+describe("The snake", () => {
+
+    it('should contain some elements', () => {
+        let snake = Snake(2);
+        snake.addElement(5, 4);
+        snake.addElement(6, 1);
+
+        let elements = snake.getElements();
+
+        expect(elements).toEqual([
+            {x: 5, y: 4},
+            {x: 6, y: 1}
+        ]);
+    });
+
+    it('should contain other elements', () => {
+        let snake = Snake(2);
+        snake.addElement(2, 1);
+        snake.addElement(8, 1);
+
+        let elements = snake.getElements();
+
+        expect(elements).toEqual([
+            {x: 2, y: 1},
+            {x: 8, y: 1}
+        ]);
+    });
+
+    it('should move elements', () => {
+        let snake = Snake(2);
+        snake.addElement(2, 1);
+        snake.addElement(8, 2);
+        snake.addElement(7, 3);
+
+        let elements = snake.getElements();
+
+        expect(elements).toEqual([
+            {x: 8, y: 2},
+            {x: 7, y: 3}
+        ]);
+    });
+
+    it('should move some more elements', () => {
+        let snake = Snake(2);
+        snake.addElement(2, 1);
+        snake.addElement(8, 2);
+        snake.addElement(7, 3);
+        snake.addElement(4, 4);
+
+        let elements = snake.getElements();
+
+        expect(elements).toEqual([
+            {x: 7, y: 3},
+            {x: 4, y: 4}
+        ]);
+    });
+
+
+    it('should create snake with a size of 3', () => {
+        let snake = Snake(3);
+        snake.addElement(2, 1);
+        snake.addElement(8, 2);
+        snake.addElement(3, 3);
+
+        let elements = snake.getElements();
+
+        expect(elements).toEqual([
+            {x: 2, y: 1},
+            {x: 8, y: 2},
+            {x: 3, y: 3},
+        ]);
+    });
+
+    it('should shuffle array with snake of length 3', () => {
+        let snake = Snake(3);
+        snake.addElement(2, 1);
+        snake.addElement(8, 2);
+        snake.addElement(3, 3);
+        snake.addElement(7, 4);
+
+        let elements = snake.getElements();
+
+        expect(elements).toEqual([
+            {x: 8, y: 2},
+            {x: 3, y: 3},
+            {x: 7, y: 4},
+        ]);
+    });
+});
