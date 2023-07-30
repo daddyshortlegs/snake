c7793d060ca31dd696f329a880b66e43f6607de1
andys <andysmith836@gmail.com>
2023-07-30 15:26:37 +0100

    Get snake moving in all directions

--- game.js
+++ game.js
@@ -2,8 +2,44 @@ function Game() {
     let x = 0;
     let y = 0;
 
+    let xDirection = 1;
+    let yDirection = 0;
+
     function tick() {
-        x += 1;
+        x += xDirection;
+        if (x > 3) {
+            x = 0;
+        } else if (x < 0) {
+            x = 3;
+        }
+
+        y += yDirection;
+        if (y > 3) {
+            y = 0;
+        } else if (y < 0) {
+            y = 3;
+        }
+
+    }
+
+    function down() {
+        xDirection = 0;
+        yDirection = 1;
+    }
+
+    function up() {
+        xDirection = 0;
+        yDirection = -1;
+    }
+
+    function left() {
+        xDirection = -1;
+        yDirection = 0;
+    }
+
+    function right() {
+        xDirection = 1;
+        yDirection = 0;
     }
 
     function getX() {
@@ -14,7 +50,7 @@ function Game() {
         return y;
     }
 
-    return { tick, getX, getY };
+    return { tick, down, up, left, right, getX, getY };
 };
 
 module.exports = { Game };
--- game.spec.js
+++ game.spec.js
@@ -29,4 +29,168 @@ describe("Snake", () => {
         expect(theGame.getY()).toEqual(0);
     });
 
+    it('should move right thrice', () => {
+        theGame.tick();
+        theGame.tick();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(3);
+        expect(theGame.getY()).toEqual(0);
+    });
+
+    it('should wrap back to start when past end of world', () => {
+        theGame.tick();
+        theGame.tick();
+        theGame.tick();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(0);
+        expect(theGame.getY()).toEqual(0);
+    });
+
+    it('should move down', () => {
+        theGame.down();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(0);
+        expect(theGame.getY()).toEqual(1);
+    });
+
+    it('should move down twice', () => {
+        theGame.down();
+        theGame.tick();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(0);
+        expect(theGame.getY()).toEqual(2);
+    });
+
+    it('should move down thrice', () => {
+        theGame.down();
+        theGame.tick();
+        theGame.tick();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(0);
+        expect(theGame.getY()).toEqual(3);
+    });
+
+    it('should move down and wrap back to top', () => {
+        theGame.down();
+        theGame.tick();
+        theGame.tick();
+        theGame.tick();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(0);
+        expect(theGame.getY()).toEqual(0);
+    });
+
+    it('should move left', () => {
+        theGame.left();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(3);
+        expect(theGame.getY()).toEqual(0);
+    });
+
+    it('should move left twice', () => {
+        theGame.left();
+        theGame.tick();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(2);
+        expect(theGame.getY()).toEqual(0);
+    });
+
+    it('should move left thrice', () => {
+        theGame.left();
+        theGame.tick();
+        theGame.tick();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(1);
+        expect(theGame.getY()).toEqual(0);
+    });
+
+    it('should move up', () => {
+        theGame.up();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(0);
+        expect(theGame.getY()).toEqual(3);
+    });
+
+    it('should move up', () => {
+        theGame.left();
+        theGame.up();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(0);
+        expect(theGame.getY()).toEqual(3);
+    });
+
+    it('should move around a bit', () => {
+        theGame.tick();
+        theGame.down();
+        theGame.tick();
+        theGame.left();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(0);
+        expect(theGame.getY()).toEqual(1);
+    });
+
+    it('should move right', () => {
+        theGame.right();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(1);
+        expect(theGame.getY()).toEqual(0);
+    });
+
+    it('should move right and down', () => {
+        theGame.right();
+        theGame.tick();
+        theGame.down();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(1);
+        expect(theGame.getY()).toEqual(1);
+    });
+
+    it('should move right and down more', () => {
+        theGame.right();
+        theGame.tick();
+        theGame.down();
+        theGame.tick();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(1);
+        expect(theGame.getY()).toEqual(2);
+    });
+
+    it('should move right and down more then left', () => {
+        theGame.right();
+        theGame.tick();
+        theGame.down();
+        theGame.tick();
+        theGame.tick();
+        theGame.left();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(0);
+        expect(theGame.getY()).toEqual(2);
+    });
+
+    it('should move down the right', () => {
+        theGame.down();
+        theGame.tick();
+        theGame.right();
+        theGame.tick();
+
+        expect(theGame.getX()).toEqual(1);
+        expect(theGame.getY()).toEqual(1);
+    });
+
 });
